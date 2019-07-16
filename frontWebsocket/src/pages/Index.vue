a594a3f6b1ba0116149caefef33dc552545726a7
Leonardo Casales <leonardo@smartnet.com.ar>
2019-07-16 00:16:53 -0300

    ejemplo de front y back funcionando comunicados con laravel-websockets

---
+++ frontWebsocket/src/pages/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+import Echo from 'laravel-echo'
+window.Pusher = require('pusher-js')
+
+export default {
+  name: 'PageIndex',
+  mounted () {
+    window.Echo = new Echo({
+      broadcaster: 'pusher',
+      key: 'ASDASD2121',
+      wsHost: window.location.hostname,
+      wsPort: 6001,
+      disableStats: true
+    })
+    window.Echo.channel('home').listen('NewMessage', (e) => {
+      console.log(e.message)
+    })
+  }
+}
+</script>
